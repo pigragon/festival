8f790820200909d059caf10e41bb92576cffc1a4
pigragon <pigragon@pigragon.kr>
2026-03-30 12:53:40 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE = {
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 8,
-  postPerPage: 4,
+  postPerPage: 8,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
